b7824b477162fdd7bd824f6de1fb5811cd89aafd
prabhanjan-kr <prabhanjankr@protonmail.com>
2018-09-25 23:22:39 +0530

    Connection test with firebase realtime database done

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -18,8 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        
         FirebaseApp.configure()
+        let myDataBase = Database.database().reference()
+        myDataBase.setValue("Writing from Flash chat for ios")
+        
         
         return true
     }
